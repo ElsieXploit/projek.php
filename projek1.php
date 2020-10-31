1e9dc4cfecae9071f9cd7ba8982fd5a21296e1fb
ElsieXploit <73713400+ElsieXploit@users.noreply.github.com>
2020-10-31 11:53:43 +0700

    Create projek1.php

---
+++ projek1.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// FAKE TOOLS, ini hanya untuk latihan
+    echo "masukan nomor korban: ";
+
+    $input_nomor =
+
+    fopen("php://stdin","r");
+
+    $nama = trim(fgets($input_nomor));
+
+    for($i = 0; $i < 10;){
+
+    echo "sedang mengirim trojan ke nomor $nama\n";
+
+    };
+
+?>
+
